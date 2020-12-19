348821dab6d69f492790bfd4028ec3fbb6bc5462
Rajesh Jha <jharajesh035@gmail.com>
2020-12-19 18:05:33 +0530

    mycarousal title-message scroll-down

--- src/App.js
+++ src/App.js
@@ -8,10 +8,19 @@ import './App.css';
 // Navbar
 import MyNavbar from "./components/my-navbar/mynavbar.component";
 
+//Carousal
+import MyCarousal from "./components/my-carousal/mycarousal.component";
+
+//Title Message
+import MyTitleMessage from "./components/title-message/title-message.component";
+
+
 const App = () => {
   return (
     <div className="App">
       <MyNavbar />
+      <MyCarousal />
+      <MyTitleMessage />
     </div>
   );
 }

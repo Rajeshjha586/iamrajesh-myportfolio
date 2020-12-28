5943524b36d064a28afdf3dd50a878a60861aaf1
Rajesh Jha <jharajesh035@gmail.com>
2020-12-28 19:34:18 +0530

    Final Submission

--- src/components/projects-timeline/projects-timeline.component.jsx
+++ src/components/projects-timeline/projects-timeline.component.jsx
@@ -7,6 +7,8 @@ import L_SharingWhiteBoard from "../../assets/img/projects/WhiteBoard.png";
 import L_RealTimeChatApp from "../../assets/img/projects/realtimechat.PNG"
 import L_TaskListApp from "../../assets/img/projects/TaskListApp.PNG"
 import L_CalculatorApp from "../../assets/img/projects/Calculator.PNG"
+import L_Excel from "../../assets/img/projects/ExcelClone.png"
+import L_Histagram from "../../assets/img/projects/histagram.PNG"
 
 import Accordion from "react-bootstrap/Accordion";
 import Card from "react-bootstrap/Card";
@@ -21,7 +23,7 @@ import L_HTML5 from "../../assets/img/skills/html-5.svg";
 import L_CSS3 from "../../assets/img/skills/css3.svg";
 import L_BOOTSTRAP4 from "../../assets/img/skills/bootstrap-4.svg";
 
-import L_DIGITAL_OCEAN from "../../assets/img/skills/digital-ocean.svg";
+
 import L_GIT from "../../assets/img/skills/github-api.svg";
 import L_MATERIALUI from "../../assets/img/skills/material-ui-1.svg";
 import L_HEROKU from "../../assets/img/skills/heroku.svg";
@@ -37,7 +39,269 @@ const TimeLine = () => {
       <Timeline>
         <Events>
         
-        {/* Project: Task List App */}
+        {/* Project: Instagram_Clone */}
+        <ImageEvent
+            date="28/12/2k20"
+            className="text-center"
+            text="Instagram Clone"
+            src={L_Histagram}
+            alt="Histagram"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> This Project clone of Instagram ,where i make REST-API and used MERN stack technology.
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>Authentication Features</li>
+                          <li>Same Feature of Insta</li>
+                          <li>Used cloud service to pic storage</li>
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_HTML5}
+                                alt="HTML 5"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              HTML5
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CSS3}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_REACT}
+                                alt="React"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              React.js
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_EXPRESS}
+                                alt="Express.js"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Express.js
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_NODE_JS}
+                                alt="Node.js"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Node.js
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_MONGODB}
+                                alt="Node.js"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Node.js
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_MATERIALUI}
+                                alt="Material-UI"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Material-UI
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_HEROKU}
+                                alt="Heroku"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                                Heroku
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_GIT}
+                                alt="github"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                                GitHub
+                            </span>
+                          </li>
+                          
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                <UrlButton
+                  href="https://histagran.herokuapp.com/signin"
+                  target="_blank"
+                >
+                  SEE LIVE
+                </UrlButton>
+                <UrlButton
+                  href="https://github.com/Rajeshjha586/Instagram_Clone"
+                  target="_blank"
+                >
+                  SOURCE CODE
+                </UrlButton>
+                
+              </div>
+            </div>
+          </ImageEvent>
+        
+
+        {/* Project: Excel Clone*/}
+        <ImageEvent
+            date="10/08/2k20"
+            className="text-center"
+            text="Excel Clone"
+            src={L_Excel}
+            alt="Excel Clone App"
+          >
+            <div className="d-flex justify-content-between flex-column mt-1">
+              <div>
+                <Accordion>
+                  <Card>
+                    <Accordion.Toggle
+                      as={Card.Header}
+                      eventKey="0"
+                      className="p-2 text-center accordian-main"
+                    >
+                      PROJECT DETAILS
+                    </Accordion.Toggle>
+
+                    <Accordion.Collapse eventKey="0" className="text-left">
+                      <Card.Body>
+                        <strong>Description:</strong> This is a Excel Clone project work same as Google Excel sheet created by Electron.js.
+                        <hr />
+                        <strong>Features:</strong>
+                        <ul className="list-styles pt-1">
+                          <li>Use the Formula</li>
+                          <li>Save the whole record</li>
+                          <li>Chnage the size and color of the text</li>
+                          <li>Powered by React and Material UI</li>
+                          <li>Respoisive Design</li>
+                        </ul>
+                        <hr />
+                        <strong>Tech used:</strong>
+                        <ul>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_HTML5}
+                                alt="HTML 5"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              HTML5
+                            </span>
+                          </li>
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_CSS3}
+                                alt="CSS 3"
+                                rounded
+                                className="image-style m-1"
+                              ></Image>{" "}
+                              CSS3
+                            </span>
+                          </li>
+                         
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_MATERIALUI}
+                                alt="Material-UI"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                              Material-UI
+                            </span>
+                          </li>
+
+                          <li>
+                            <span className="p-2">
+                              <Image
+                                src={L_GIT}
+                                alt="github"
+                                rounded
+                                className="image-style1 m-1"
+                              ></Image>{" "}
+                                GitHub
+                            </span>
+                          </li>
+
+
+                          
+                        </ul>
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
+              <div className="d-flex justify-content-between flex-nowrap text-center">
+                
+                <UrlButton
+                  href="https://github.com/Rajeshjha586/Excelclone"
+                  target="_blank"
+                >
+                  SOURCE CODE
+                </UrlButton>
+                
+              </div>
+            </div>
+          </ImageEvent>
+
+
+        {/* Project: Calculator App */}
         <ImageEvent
             date="12/04/2k20"
             className="text-center"
@@ -157,7 +421,6 @@ const TimeLine = () => {
               </div>
             </div>
           </ImageEvent>
-
 
 
         {/* Project: Task List App */}
@@ -423,10 +686,9 @@ const TimeLine = () => {
             </div>
           </ImageEvent>
 
- 
 
-          {/* Project: Sharing White Board */}
-          <ImageEvent
+        {/* Project: Sharing White Board */}
+        <ImageEvent
             date="07/12/2019"
             className="text-center"
             text="Sharing White Board App"
